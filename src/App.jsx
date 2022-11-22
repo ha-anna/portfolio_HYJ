a108412021a2515edbdd265e6548f1d6970d7faf
Ha Anna <its.haanna@gmail.com>
2022-11-22 14:35:46 +0900

    Upload: Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
 import React, { useState } from "react";
+import { BrowserRouter, Link, Route, Routes, Navigate } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <h2>hi</h2>
-    </div>
+    <>
+      <BrowserRouter>
+        <header>
+          <Link to="/">
+            <img src="./assets/logo.webp" alt="" />
+          </Link>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/overview">Overview</Link>
+              </li>
+              <li>
+                <Link to="/project">Project</Link>
+              </li>
+              <li>
+                <Link to="/reportage">Reportage</Link>
+              </li>
+              <li>
+                <Link to="/film">Film</Link>
+              </li>
+              <li>
+                <Link to="/commissioned">Commissioned</Link>
+              </li>
+              <li>
+                <Link to="/exhibiition">Exhibition</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="https://www.facebook.com/youjun.ha"></Link>
+              </li>
+              <li>
+                <Link to="https://www.instagram.com/studio_siseon/"></Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </BrowserRouter>
+    </>
   );
 }
 
