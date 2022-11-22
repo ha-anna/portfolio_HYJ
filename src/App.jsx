7bbed5ee065561fb483f456a5cd8f0c975f3a059
Ha Anna <its.haanna@gmail.com>
2022-11-22 13:37:30 +0900

    Initial upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>hi</h2>
+    </div>
+  );
+}
+
+export default App;
