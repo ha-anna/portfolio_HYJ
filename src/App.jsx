5e2c69c60e2f890c5a4be57dad587d3519605582
Ha Anna <its.haanna@gmail.com>
2022-12-17 17:37:25 +0900

    Upload: webp files

--- src/App.jsx
+++ src/App.jsx
@@ -20,13 +20,13 @@ import Concrete from "./pages/Project/Concrete";
 import Err0r from "./pages/Project/Err0r";
 import Living_And_Gone from "./pages/Project/Living_And_Gone";
 
-// // REPORTAGE PAGES
-// import Neoliberalism from "./pages/Reportage/Neoliberalism";
-// import LGBTQ_In_Warsaw from "./pages/Reportage/LGBTQ_In_Warsaw";
-// import Uprising from "./pages/Reportage/Uprising";
-// import Independence_Day from "./pages/Reportage/Independence_Day";
-// import Dont_Look_Back_In_Anger from "./pages/Reportage/Dont_Look_Back_In_Anger";
-// import Issues from "./pages/Reportage/Issues";
+// REPORTAGE PAGES
+import Neoliberalism from "./pages/Reportage/Neoliberalism";
+import LGBTQ_In_Warsaw from "./pages/Reportage/LGBTQ_In_Warsaw";
+import Uprising from "./pages/Reportage/Uprising";
+import Independence_Day from "./pages/Reportage/Independence_Day";
+import Dont_Look_Back_In_Anger from "./pages/Reportage/Dont_Look_Back_In_Anger";
+import Issues from "./pages/Reportage/Issues";
 
 // FILM PAGES
 import Film from "./pages/Film/Film";
@@ -58,7 +58,7 @@ function App() {
             <Route path="/concrete" element={<Concrete />} />
             <Route path="/err0r" element={<Err0r />} />
             <Route path="/living-and-gone" element={<Living_And_Gone />} />
-            {/* <Route path="/neoliberalism" element={<Neoliberalism />} />
+            <Route path="/neoliberalism" element={<Neoliberalism />} />
             <Route path="/lgbtq-in-warsaw" element={<LGBTQ_In_Warsaw />} />
             <Route path="/8-1-uprising" element={<Uprising />} />
             <Route path="/independence-day" element={<Independence_Day />} />
@@ -66,7 +66,7 @@ function App() {
               path="/dont-look-back-in-anger"
               element={<Dont_Look_Back_In_Anger />}
             />
-            <Route path="/issues" element={<Issues />} /> */}
+            <Route path="/issues" element={<Issues />} />
             <Route path="/film" element={<Film />} />
             <Route path="/commissioned" element={<Commissioned />} />
             <Route path="/exhibition" element={<Exhibition />} />
