82df10fad95d77eabe01957b2a8ca3673528ae12
Ha Anna <its.haanna@gmail.com>
2022-12-17 13:25:27 +0900

    Upload: Project pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,17 @@ import ErrorPage from "./pages/ErrorPage";
 import Overview from "./pages/Overview/Overview";
 
 // PROJECT PAGES
+import New_Galateia from "./pages/Project/New_Galateia/New_Galateia";
 import Kolorowa from "./pages/Project/Kolorowa/Kolorowa";
+import Minus from "./pages/Project/Minus/Minus";
+import Concrete from "./pages/Project/Concrete/Concrete";
+import Err0r from "./pages/Project/Err0r/Err0r";
+import Living_And_Gone from "./pages/Project/Living_And_Gone/Living_And_Gone";
 
 // REPORTAGE PAGES
 
 import Reportage from "./pages/Reportage";
-import Film from "./pages/Film";
+import Film from "./pages/Film/Film";
 import Commissioned from "./pages/Commissioned";
 
 import Exhibition from "./pages/Exhibition";
@@ -32,12 +37,12 @@ function App() {
           <Routes>
             <Route path="/" element={<Kolorowa />} />
             <Route path="/overview" element={<Overview />} />
-            <Route path="/new-galateia" element={<ErrorPage />} />
+            <Route path="/new-galateia" element={<New_Galateia />} />
             <Route path="/kolorowa" element={<Kolorowa />} />
-            <Route path="/minus" element={<ErrorPage />} />
-            <Route path="/concrete" element={<ErrorPage />} />
-            <Route path="/err0r" element={<ErrorPage />} />
-            <Route path="/living-and-gone" element={<ErrorPage />} />
+            <Route path="/minus" element={<Minus />} />
+            <Route path="/concrete" element={<Concrete />} />
+            <Route path="/err0r" element={<Err0r />} />
+            <Route path="/living-and-gone" element={<Living_And_Gone />} />
             <Route path="/neoliberalism" element={<ErrorPage />} />
             <Route path="/lgbt-in-warsaw" element={<ErrorPage />} />
             <Route path="/8-1-uprising" element={<ErrorPage />} />
---
+++ src/pages/Project/Err0r/Err0r.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Link, generatePath } from "react-router-dom";
+
+function Err0r() {
+  const images = Object.keys(import.meta.glob("./assets/*", { as: "raw" }));
+  const imagesHtml = images.map((url, i) => (
+    <Link to={`${url.slice(9)}`} key={i}>
+      <img src={`./assets/Err0r/${url.slice(9)}`} alt="" key={i} id={`${i}`} />
+    </Link>
+  ));
+
+  return (
+    <>
+      <div className="img-grid">{imagesHtml}</div>
+    </>
+  );
+}
+
+export default Err0r;
