6f4ae3b3919bb5fd81e305670011666dd66a426f
Ha Anna <its.haanna@gmail.com>
2022-12-17 16:07:07 +0900

    Upload: test

--- src/pages/Reportage/Uprising.jsx
+++ src/pages/Reportage/Uprising.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link, generatePath } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Uprising() {
   const images = Object.keys(
