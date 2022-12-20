24a73c899a66e810afe83260884a07cd656ad1ed
Ha Anna <its.haanna@gmail.com>
2022-12-20 20:29:34 +0900

    Upload: Film page setup

--- src/pages/Film/Film.jsx
+++ src/pages/Film/Film.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Film() {
   return (
-    <>
+    <div className="film">
       <h2 className="page-title border-bottom">Film</h2>
-    </>
+    </div>
   );
 }
 
