44a2431f38005058e17be58cdaf280f435d4ac5b
Ha Anna <its.haanna@gmail.com>
2022-12-20 20:31:43 +0900

    Upload: Routing

---
+++ src/pages/Film/Subscribe_My_Dream.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Subscribe_My_Dream() {
+  return (
+    <div className="film">
+      <h2 className="page-title border-bottom">Film</h2>
+    </div>
+  );
+}
+
+export default Subscribe_My_Dream;
