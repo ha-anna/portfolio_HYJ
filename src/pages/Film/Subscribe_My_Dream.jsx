ca0616f226f814e88819e0b37c9516e57d8e0294
Ha Anna <its.haanna@gmail.com>
2022-12-22 11:55:33 +0900

    Upload: single film page

--- src/pages/Film/Subscribe_My_Dream.jsx
+++ src/pages/Film/Subscribe_My_Dream.jsx
@@ -3,16 +3,79 @@ import React from "react";
 function Subscribe_My_Dream() {
   return (
     <div className="film">
-      <iframe
-        width="100%"
-        height="400"
-        src="https://www.youtube.com/embed/lwdoVX1YUWE"
-        title="YouTube video player"
-        frameBorder="0"
-        allow=""
-        allowFullScreen
-      ></iframe>
-      <span className="description">Subscribe my dream_Eng Sub</span>
+      <h2 className="page-title border-bottom">Subscribe My Dream</h2>
+      <article className="film-field">
+        <iframe
+          src="https://www.youtube.com/embed/lwdoVX1YUWE"
+          title="Subscribe My Dream movie (ENG sub)"
+          frameBorder="0"
+          allowFullScreen
+          className="video-big"
+        ></iframe>
+        <span className="description">Subscribe my dream_Eng Sub</span>
+
+        <p className="bold-big margin-top">Description</p>
+        <p className="text-eng">
+          <span className="bold">Title:</span> Subscribe my dream
+        </p>
+        <p className="text-eng">
+          <span className="bold">Duration:</span> 25min
+        </p>
+        <p className="text-eng">
+          <span className="bold">Genre:</span> Drama, Romance
+        </p>
+        <p className="text-eng">
+          <span className="bold">Synopsis:</span> Bonding with people don’t
+          possess many is greater than it with those who have all. Maciek is a
+          beginning actor and Agata is a failed singer. They met on tinder and
+          started to talk about things never fulfilled or never have.
+        </p>
+        <p className="description margin-top">
+          This motion picture is protected under the laws of Korea and other
+          countries. Unauthorized duplication, distribution, or exhibition may
+          result in civil liability and criminal prosecution. “Dziwny jest ten
+          świat” is under negotiation of copyright in 2020. © 2020 Seoul, Korea
+        </p>
+      </article>
+      <div className="border-bottom"></div>
+      <article className="film-field">
+        <iframe
+          src="https://www.youtube-nocookie.com/embed/gaC7QEIzAb4"
+          title="Subscribe My Dream movie (KOR sub)"
+          frameBorder="0"
+          allowFullScreen
+          className="video-big"
+        ></iframe>
+        <span className="description">Subscribe my dream_Korean Sub</span>
+
+        <p className="bold-big margin-top">상영정보</p>
+        <p className="text-kr">
+          <span className="bold">제목:</span> 꿈도 구독이 되나요
+        </p>
+        <p className="text-kr">
+          <span className="bold">상영시간:</span> 25분
+        </p>
+        <p className="text-kr">
+          <span className="bold">장르:</span> 드라마, 로맨스
+        </p>
+        <p className="text-kr">
+          <span className="bold">시놉시스:</span>  갖지 못한 자들의 한 여름밤
+          구설수. 갖을 수 없어서 늘 ‘빌리거나’, ‘렌트’하거나 ‘구독’해아만 하는
+          이들의 이야기. 배우 지망생 마첵은 배우가 되기 위해 바르샤바에
+          상경한다. 삭막한 도시에서 사람의 향기가 그리울 쯤, 틴더를 통해
+          아가타를 만나게 된다. 아가타는 한 때 가수가 되고 싶다는 큰 포부를
+          가졌지만 현실적 문제에 부딪혀 식당 종업원으로 일하며 간간히 살아간다.
+          둘은 데이트를 위해 만났지만 아가타는 마첵의 순진한 포부에 질색한다.
+          낭만적 이상주의자와 퇴폐한 현실주의자의 데이트는 한밤 중의 잡담으로
+          이어진다.
+        </p>
+        <p className="description margin-top">
+          This motion picture is protected under the laws of Korea and other
+          countries. Unauthorized duplication, distribution, or exhibition may
+          result in civil liability and criminal prosecution. “Dziwny jest ten
+          świat” is under negotiation of copyright in 2020. © 2020 Seoul, Korea
+        </p>
+      </article>
     </div>
   );
 }
