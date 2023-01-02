b0b3973a375fed53563706d7663ee271e2cf857f
Ha Anna <its.haanna@gmail.com>
2023-01-02 23:32:03 +0900

    Upload: img files

--- src/pages/Film/Film.jsx
+++ src/pages/Film/Film.jsx
@@ -17,7 +17,7 @@ function Film() {
           </Link>
         </section>
         <img
-          src="./assets/Exhibition/Dont_Skip_It/1-1.webp"
+          src="./assets/Exhibition/Dont_Skip_It/1.png"
           alt="Subscribe My Dream poster"
           className="film-img"
         />
