7240f0acacf16471f1f10bd5bee157d08a6f0655
Ha Anna <its.haanna@gmail.com>
2023-01-04 15:27:36 +0900

    Update: Added video to Kyodong page

--- src/pages/Exhibition/Kyodong.jsx
+++ src/pages/Exhibition/Kyodong.jsx
@@ -21,15 +21,25 @@ function Kyodong() {
     <ImageContext.Consumer>
       {() => {
         return (
-          <Masonry
-            items={items}
-            columnWidth={400}
-            maxColumnCount={5}
-            columnGutter={15}
-            rowGutter={10}
-            render={cards}
-            className="img-grid"
-          ></Masonry>
+          <>
+            <iframe
+              src="https://www.youtube-nocookie.com/embed/hsTrCZUybhs"
+              title="Kyodong Art Group Exhibition: Interview with Ha Youjun"
+              frameBorder="0"
+              allowFullScreen
+              data-aos="fade-up"
+              className="video-big"
+            ></iframe>
+            <Masonry
+              items={items}
+              columnWidth={400}
+              maxColumnCount={5}
+              columnGutter={15}
+              rowGutter={10}
+              render={cards}
+              className="img-grid"
+            ></Masonry>
+          </>
         );
       }}
     </ImageContext.Consumer>
