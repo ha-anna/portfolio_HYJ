46976a46e31fd92a8725d41cb1d95ac21528ac4f
Ha Anna <its.haanna@gmail.com>
2023-01-07 15:18:35 +0900

    Upload: Commissioned -> DRY

--- src/pages/Commissioned/Jungeunhae_EU.jsx
+++ src/pages/Commissioned/Jungeunhae_EU.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ImageContext from "../../ImageContext";
-import { Masonry } from "masonic";
+import ImageGrid from "../../components/ImageGrid";
 import { sortAlphaNum } from "../../Utils/sortAlphaNum";
 import { tabTitle } from "../../Utils/tabTitle";
 import { getCards, getItems } from "../../Utils/grid";
@@ -20,17 +20,7 @@ function Jungeunhae_EU() {
   return (
     <ImageContext.Consumer>
       {() => {
-        return (
-          <Masonry
-            items={items}
-            columnWidth={400}
-            maxColumnCount={5}
-            columnGutter={15}
-            rowGutter={10}
-            render={cards}
-            className="img-grid"
-          ></Masonry>
-        );
+        return <ImageGrid items={items} cards={cards} />;
       }}
     </ImageContext.Consumer>
   );
